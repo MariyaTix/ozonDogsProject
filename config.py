6a9db14f960aab2fc82eeb6fb1111452effb6e44
MariyaTix <mariya.tikhomirova.ss@gmail.com>
2024-11-06 22:05:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class YaToken:
     """Токен из яндекс полигона - https://yandex.ru/dev/disk/poligon/"""
-    token = 'y0_AgAAAAAQqhlWAADLWwAAAAEWKNU5AAAFC__2N51K1KdG0ijaVqp2Lh6Mkg'
+    token = 'y0_A.........'
